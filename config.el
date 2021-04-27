ae10ecab9756a6cbb0bb555a347ab4d8b44bb108
Phil McCredden <philmccredden@users.noreply.github.com>
2021-04-27 17:57:38 +1000

    many updates

--- config.el
+++ config.el
@@ -1,83 +1,206 @@
-(defvar efs/default-font-size 140)
-(defvar efs/default-variable-font-size 140)
+(defvar runemacs/default-font-size 140)
 
-(setq inhibit-startup-message t)  ; turn off splash screen
+    (setq inhibit-startup-message t)
 
-(tool-bar-mode -1)   ; turn tool bar off
-(menu-bar-mode -1)   ; turn menu bar off
-(scroll-bar-mode -1) ; scoll bars off
-(tooltip-mode -1)    ; Disable tooltips
-(set-fringe-mode 10) ; Give some breathing room
+    (scroll-bar-mode -1)        ; Disable visible scrollbar
+    (tool-bar-mode -1)          ; Disable the toolbar
+    (tooltip-mode -1)           ; Disable tooltips
+    (set-fringe-mode 10)        ; Give some breathing room
 
-;; Set up the visible bell
-(setq visible-bell t)
+    (menu-bar-mode -1)            ; Disable the menu bar
 
-; Display relative line numbers
-(column-number-mode)
-(global-display-line-numbers-mode t)
-(setq display-line-numbers 'relative)
+    ;; Set up the visible bell
+    (setq visible-bell t)
 
-;; Answering just 'y' or 'n' will suffice
-(defalias 'yes-or-no-p 'y-or-n-p)
+    (set-face-attribute 'default nil :font "Menlo" :height runemacs/default-font-size)
 
-;; Backup config
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+	;; Make ESC quit prompts
+	(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(require 'package)
+	;; Initialize package sources
+	(require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                       ("org" . "https://orgmode.org/elpa/")
-                       ("elpa" . "https://elpa.gnu.org/packages/")))
+	(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+				 ("org" . "https://orgmode.org/elpa/")
+				 ("elpa" . "https://elpa.gnu.org/packages/")))
 
-		       (package-initialize)
-		       (unless package-archive-contents
-		       (package-refresh-contents))
+	(package-initialize)
+	(unless package-archive-contents
+	 (package-refresh-contents))
 
-		       ;; Initialize use-package on non-Linux platforms
-		       (unless (package-installed-p 'use-package)
-		       (package-install 'use-package))
+	;; Initialize use-package on non-Linux platforms
+	(unless (package-installed-p 'use-package)
+	   (package-install 'use-package))
 
-		       (require 'use-package)
-		       (setq use-package-always-ensure t)
+	(require 'use-package)
+	(setq use-package-always-ensure t)
 
-(use-package doom-themes
-:init (load-theme 'doom-palenight t))
+	(column-number-mode)
+	(global-display-line-numbers-mode t)
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-(package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-
-; Disable line numbers for some modes
+;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-term-mode-hook
-shell-mode-hook
-treemacs-mode-hook
-eshell-mode-hook))
-(add-hook mode (lambda () (display-line-numbers-mode 0))))
+		term-mode-hook
+		shell-mode-hook
+		treemacs-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(set-face-attribute 'default nil :font "Menlo" :height efs/default-font-size)
 
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Menlo" :height efs/default-font-size)
+	(use-package command-log-mode)
 
-;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
-(custom-set-variables
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-'(package-selected-packages (quote (doom-themes use-package evil))))
-(custom-set-faces
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-)
+	(use-package ivy
+	  :diminish
+	  :bind (("C-s" . swiper)
+		 :map ivy-minibuffer-map
+		 ("TAB" . ivy-alt-done)
+		 ("C-l" . ivy-alt-done)
+		 ("C-j" . ivy-next-line)
+		 ("C-k" . ivy-previous-line)
+		 :map ivy-switch-buffer-map
+		 ("C-k" . ivy-previous-line)
+		 ("C-l" . ivy-done)
+		 ("C-d" . ivy-switch-buffer-kill)
+		 :map ivy-reverse-i-search-map
+		 ("C-k" . ivy-previous-line)
+		 ("C-d" . ivy-reverse-i-search-kill))
+	  :config
+	  (ivy-mode 1))
+
+	;; NOTE: The first time you load your configuration on a new machine, you'll
+	;; need to run the following command interactively so that mode line icons
+	;; display correctly:
+	;;
+	;; M-x all-the-icons-install-fonts
+
+	(use-package all-the-icons)
+
+	(use-package doom-modeline
+	  :init (doom-modeline-mode 1)
+	  :custom ((doom-modeline-height 15)))
+
+	(use-package doom-themes
+	  :init (load-theme 'doom-one t))
+
+	(use-package rainbow-delimiters
+	  :hook (prog-mode . rainbow-delimiters-mode))
+
+	(use-package which-key
+	  :init (which-key-mode)
+	  :diminish which-key-mode
+	  :config
+	  (setq which-key-idle-delay 1))
+
+	(use-package ivy-rich
+	  :init
+	  (ivy-rich-mode 1))
+
+	(use-package counsel
+	  :bind (("M-x" . counsel-M-x)
+		 ("C-x b" . counsel-ibuffer)
+		 ("C-x C-f" . counsel-find-file)
+		 :map minibuffer-local-map
+		 ("C-r" . 'counsel-minibuffer-history)))
+
+	(use-package helpful
+	  :custom
+	  (counsel-describe-function-function #'helpful-callable)
+	  (counsel-describe-variable-function #'helpful-variable)
+	  :bind
+	  ([remap describe-function] . counsel-describe-function)
+	  ([remap describe-command] . helpful-command)
+	  ([remap describe-variable] . counsel-describe-variable)
+	  ([remap describe-key] . helpful-key))
+
+	(use-package general
+	  :config
+	  (general-create-definer rune/leader-keys
+	    :keymaps '(normal insert visual emacs)
+	    :prefix "SPC"
+	    :global-prefix "C-SPC")
+
+	  (rune/leader-keys
+	    "t"  '(:ignore t :which-key "toggles")
+	    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+	(use-package evil
+	  :init
+	  (setq evil-want-integration t)
+	  (setq evil-want-keybinding nil)
+	  (setq evil-want-C-u-scroll t)
+	  (setq evil-want-C-i-jump nil)
+	  :config
+	  (evil-mode 1)
+	  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+	  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+	  ;; Use visual line motions even outside of visual-line-mode buffers
+	  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+	  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+	  (evil-set-initial-state 'messages-buffer-mode 'normal)
+	  (evil-set-initial-state 'dashboard-mode 'normal))
+
+	(use-package evil-collection
+	  :after evil
+	  :config
+	  (evil-collection-init))
+
+    (use-package hydra)
+
+    (defhydra hydra-text-scale (:timeout 4)
+      "scale text"
+      ("j" text-scale-increase "in")
+      ("k" text-scale-decrease "out")
+      ("f" nil "finished" :exit t))
+
+    (rune/leader-keys
+      "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Dropbox/@Work")
+    (setq projectile-project-search-path '("~/Dropbox/@Work")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; NOTE: Make sure to configure a GitHub token before using this package!
+(use-package forge)
 
 (require 'org-tempo)
+
+(global-visual-line-mode t)
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+    (defun efs/org-mode-setup ()
+	  (org-indent-mode)
+	  (variable-pitch-mode 1)
+	  (visual-line-mode 1))
--- init.el
+++ init.el
@@ -3,3 +3,16 @@
 ;; Load the config from config.org
 (require 'org) 
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-bullets hydra general all-the-icons ivy command-log-mode doom-themes use-package evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
