cda1e9d57280d5bd1074cfdaffd8f3f2a1ce28e8
Phil McCredden <philmccredden@users.noreply.github.com>
2021-04-28 20:54:03 +1000

    lots of stuff

--- config.el
+++ config.el
@@ -1,161 +1,168 @@
+; Highlights the current cursor line
+(global-hl-line-mode t)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (defvar runemacs/default-font-size 140)
 
-    (setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
 
-    (scroll-bar-mode -1)        ; Disable visible scrollbar
-    (tool-bar-mode -1)          ; Disable the toolbar
-    (tooltip-mode -1)           ; Disable tooltips
-    (set-fringe-mode 10)        ; Give some breathing room
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)            ; Disable the menu bar
 
-    (menu-bar-mode -1)            ; Disable the menu bar
+;; Set up the visible bell
+(setq visible-bell t)
 
-    ;; Set up the visible bell
-    (setq visible-bell t)
+(set-face-attribute 'default nil :font "Menlo" :height runemacs/default-font-size)
 
-    (set-face-attribute 'default nil :font "Menlo" :height runemacs/default-font-size)
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-	;; Make ESC quit prompts
-	(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Initialize package sources
+(require 'package)
 
-	;; Initialize package sources
-	(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
-	(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-				 ("org" . "https://orgmode.org/elpa/")
-				 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-	(package-initialize)
-	(unless package-archive-contents
-	 (package-refresh-contents))
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-	;; Initialize use-package on non-Linux platforms
-	(unless (package-installed-p 'use-package)
-	   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-	(require 'use-package)
-	(setq use-package-always-ensure t)
-
-	(column-number-mode)
-	(global-display-line-numbers-mode t)
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
 ;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
+(dolist (mode '(term-mode-hook
 		shell-mode-hook
 		treemacs-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
-	(use-package command-log-mode)
+(use-package command-log-mode)
 
-	(use-package ivy
-	  :diminish
-	  :bind (("C-s" . swiper)
-		 :map ivy-minibuffer-map
-		 ("TAB" . ivy-alt-done)
-		 ("C-l" . ivy-alt-done)
-		 ("C-j" . ivy-next-line)
-		 ("C-k" . ivy-previous-line)
-		 :map ivy-switch-buffer-map
-		 ("C-k" . ivy-previous-line)
-		 ("C-l" . ivy-done)
-		 ("C-d" . ivy-switch-buffer-kill)
-		 :map ivy-reverse-i-search-map
-		 ("C-k" . ivy-previous-line)
-		 ("C-d" . ivy-reverse-i-search-kill))
-	  :config
-	  (ivy-mode 1))
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-	;; NOTE: The first time you load your configuration on a new machine, you'll
-	;; need to run the following command interactively so that mode line icons
-	;; display correctly:
-	;;
-	;; M-x all-the-icons-install-fonts
+;; NOTE: The first time you load your configuration on a new machine, you'll
+;; need to run the following command interactively so that mode line icons
+;; display correctly:
+;;
+;; M-x all-the-icons-install-fonts
 
-	(use-package all-the-icons)
+(use-package all-the-icons)
 
-	(use-package doom-modeline
-	  :init (doom-modeline-mode 1)
-	  :custom ((doom-modeline-height 15)))
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
-	(use-package doom-themes
-	  :init (load-theme 'doom-one t))
+(use-package doom-themes
+  :init (load-theme 'doom-one t))
 
-	(use-package rainbow-delimiters
-	  :hook (prog-mode . rainbow-delimiters-mode))
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-	(use-package which-key
-	  :init (which-key-mode)
-	  :diminish which-key-mode
-	  :config
-	  (setq which-key-idle-delay 1))
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
 
-	(use-package ivy-rich
-	  :init
-	  (ivy-rich-mode 1))
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
 
-	(use-package counsel
-	  :bind (("M-x" . counsel-M-x)
-		 ("C-x b" . counsel-ibuffer)
-		 ("C-x C-f" . counsel-find-file)
-		 :map minibuffer-local-map
-		 ("C-r" . 'counsel-minibuffer-history)))
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
-	(use-package helpful
-	  :custom
-	  (counsel-describe-function-function #'helpful-callable)
-	  (counsel-describe-variable-function #'helpful-variable)
-	  :bind
-	  ([remap describe-function] . counsel-describe-function)
-	  ([remap describe-command] . helpful-command)
-	  ([remap describe-variable] . counsel-describe-variable)
-	  ([remap describe-key] . helpful-key))
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
-	(use-package general
-	  :config
-	  (general-create-definer rune/leader-keys
-	    :keymaps '(normal insert visual emacs)
-	    :prefix "SPC"
-	    :global-prefix "C-SPC")
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
 
-	  (rune/leader-keys
-	    "t"  '(:ignore t :which-key "toggles")
-	    "tt" '(counsel-load-theme :which-key "choose theme")))
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
 
-	(use-package evil
-	  :init
-	  (setq evil-want-integration t)
-	  (setq evil-want-keybinding nil)
-	  (setq evil-want-C-u-scroll t)
-	  (setq evil-want-C-i-jump nil)
-	  :config
-	  (evil-mode 1)
-	  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-	  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-	  ;; Use visual line motions even outside of visual-line-mode buffers
-	  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-	  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-	  (evil-set-initial-state 'messages-buffer-mode 'normal)
-	  (evil-set-initial-state 'dashboard-mode 'normal))
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
-	(use-package evil-collection
-	  :after evil
-	  :config
-	  (evil-collection-init))
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
-    (use-package hydra)
+(use-package hydra)
 
-    (defhydra hydra-text-scale (:timeout 4)
-      "scale text"
-      ("j" text-scale-increase "in")
-      ("k" text-scale-decrease "out")
-      ("f" nil "finished" :exit t))
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
 
-    (rune/leader-keys
-      "ts" '(hydra-text-scale/body :which-key "scale text"))
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(setq indo-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
 (use-package projectile
   :diminish projectile-mode
@@ -179,6 +186,11 @@
 ;; NOTE: Make sure to configure a GitHub token before using this package!
 (use-package forge)
 
+;; set up org mobile mode for ipad
+(setq org-directory "~/Dropbox/org")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
 (require 'org-tempo)
 
 (global-visual-line-mode t)
@@ -200,7 +212,30 @@
 
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
-    (defun efs/org-mode-setup ()
-	  (org-indent-mode)
-	  (variable-pitch-mode 1)
-	  (visual-line-mode 1))
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+;; easy window switchingo
+(use-package switch-window
+  :ensure t
+  :bind
+  ;; default C-x o is other-window
+  ;; default C-x C-o is delete-blank-lines
+  (("C-x o" . switch-window)
+   ("C-x C-o" . switch-window))
+  :config
+  (setq switch-window-multiple-frames t)
+  (setq switch-window-shortcut-style 'qwerty)
+  ;; when Emacs is run as client, the first shortcut does not appear
+  ;; "x" acts as a dummy; remove first entry if not running server
+  (setq switch-window-qwerty-shortcuts '("x" "a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "r" "u" "i" "o" "q" "t" "y" "p"))
+  (setq switch-window-increase 3))
+
+(setq org-src-fontify-natively t)
+
+(org-babel-do-load-languages
+	 'org-babel-load-languages
+	 '((emacs-lisp . t)
+	   (python . t)))
--- init.el
+++ init.el
@@ -9,10 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets hydra general all-the-icons ivy command-log-mode doom-themes use-package evil)))
+   '(switch-window ace-window smex Smex org-bullets hydra general all-the-icons ivy command-log-mode doom-themes use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
